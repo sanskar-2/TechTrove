5ddc6278ecb985ec0d3c6bd04b41089678d2eb20
sanskar-2 <jainsanskar788@gmail.com>
2024-04-04 17:14:14 +0530

    intial commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
-#pip install -r requirements.txt
-#python 3.9 manage.py collectstatic
+echo "BUILD START"
+python 3.9 -m pip install -r requirements.txt
+python 3.9 manage.py collectstatic --noinput --clear
+echo  "BUILD END"
 
-
-# Use the full path to the pip executable
-/usr/bin/python3.9 -m pip install -r requirements.txt
-# Use the full path to the python executable
-/usr/bin/python3.9 manage.py collectstatic
