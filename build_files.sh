50988b4b7f5d55b2fd56a3a022e26c10eae1ff2e
sanskar-2 <jainsanskar788@gmail.com>
2024-04-17 15:08:42 +0530

    intial commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
-echo "BUILD START"
-python 3.9 -m pip install -r requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
-echo  "BUILD END"
+
+python 3.9.12-m pip install -r requirements.txt
+python D:\juypter\python3.9.12 manage.py
 
