d399be5289cc75d3741a6b7d88f7110612ee2f33
sanskar-2 <jainsanskar788@gmail.com>
2024-04-24 11:45:40 +0530

    intial commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-C:\Users\SANSKAR JAIN\AppData\Local\Microsoft\WindowsApps\python\python3.9 -m pip install -r requirements.txt
-C:\Users\SANSKAR JAIN\AppData\Local\Microsoft\WindowsApps\python\python3.9 manage.py
+C:\\Users\\SANSKAR JAIN\\AppData\\Local\\Microsoft\\WindowsApps\\python\\python3.9 -m pip install -r requirements.txt
+C:\\Users\\SANSKAR JAIN\\AppData\\Local\\Microsoft\\WindowsApps\\python\\python3.9 manage.py
